95fe962fa4321ed5f1e83f648fb3c6d5998a4b35
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2021-05-11 16:17:34 +0000

    :iphone: :seedling: Mobile First :sparkles: :iphone:

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ const burgerMenu = document.querySelector(".burger-menu");
 burgerMenu.addEventListener("click", () => {
   navbar.classList.toggle("active");
 })
+
+const URLbtn = document.querySelector(".URLbtn");
+const url = document.querySelector("#url");
+const SpanURL = document.querySelector(".error");
+
+URLbtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const urlValue = url.value;
+  const pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  if (pattern.test(urlValue)) {
+    alert("Url is valid");
+    return true;
+  } else {
+    url.classList.add("error");
+    SpanURL.style.display = "block";
+  }
+})
